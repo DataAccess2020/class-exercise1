f643ca9d2d1baa3ddfb1675449a30f3fd150db35
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-02-13 12:20:29 +0100

    Statistics 1: tibble w/ proportions

---
+++ 04_statistics.R
@@ -0,0 +1,19 @@
+##Statistics
+
+library(rio)
+library(tidyverse)
+
+voteinc <- import("voteinc.dta")
+
+#We have different levels of income (expressed in deciles, from 1 to 10) and we want to know
+#if there's a relationship between income and vote; firstly we observe the proportions of vote 
+#for each group:
+
+voteinc %>%
+  group_by(hinctnta) %>%
+  summarize(
+    voteprop = mean(vote, na.rm = T)
+  )
+
+#as we can see, they are different for each level of income
+
